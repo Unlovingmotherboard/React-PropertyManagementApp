3d36dabc59e8c7c4f17e3294747ddf0c673bcef2
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-11-08 11:20:48 -0600

    Removed npm packages i didn't need from boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 
+//Store --> Globalized State
+
+//ACTION
+
+//REDUCER
+
+
 render(<App />, document.getElementById('root'));
