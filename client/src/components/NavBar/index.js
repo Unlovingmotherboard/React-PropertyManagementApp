69dd2c9ddaf31e0a309234b949a36bbbd5cbe8d6
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-12-02 10:00:56 -0600

    Still testing bugs

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter , Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { Navbar, Dropdown, Button } from "react-materialize"
 import "./style.css"
 
@@ -36,48 +36,52 @@ function NavBar(props) {
         <div>
             <Navbar alignLinks="right">
                 <Link to="/">Home Page</Link>
-               {
-                   props.type === "Tenant" ?
-                   <Dropdown trigger={<a>Tentnat</a>}>
                 {
-                     <Button onClick={logoutTenant}>Logout</Button>
-                }
-                </Dropdown> : 
-                   props.type === 'Manager' ? 
-                   <Dropdown trigger={<a>Manager</a>}>
-                {   
-                    <Button onClick={logoutManager}>Logout</Button>
-                    
-                }
-                </Dropdown>
-                :
+                    props.type === "Tenant" ?
+                        <Dropdown trigger={<a>Tentnat</a>}>
+                            {
+                                <Button onClick={logoutTenant}>Logout</Button>
+                            }
+                        </Dropdown> :
+                        props.type === 'Manager' ?
+                            <Dropdown trigger={<a>Manager</a>}>
+                                {
+                                    <React.Fragment>
+                                        <Link to="/Manager">Manager Page</Link>
+                                        <Button onClick={logoutManager}>Logout</Button>
+                                     </React.Fragment>
 
-                   <Dropdown trigger={<a>Sign Up</a>}>
-                {
-                    <React.Fragment>
-                                <Link to="/SignUp">Tenant Sign Up</Link>
-                                <Link to="/Manager/SignUp">Manager Sign Up</Link>
-                                <Link to="/Login">Tenant Login</Link>
-                                <Link to="/Manager/Login">Manager Login</Link>
-                        </React.Fragment>
+
+                                }
+                            </Dropdown>
+                            :
+
+                            <Dropdown trigger={<a>Sign Up</a>}>
+                                {
+                                    <React.Fragment>
+                                        <Link to="/SignUp">Tenant Sign Up</Link>
+                                        <Link to="/Manager/SignUp">Manager Sign Up</Link>
+                                        <Link to="/Login">Tenant Login</Link>
+                                        <Link to="/Manager/Login">Manager Login</Link>
+                                    </React.Fragment>
+                                }
+                            </Dropdown>
                 }
-                </Dropdown>
-               } 
             </Navbar>
         </div>
     );
 }
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-        logout,
-    },
-    dispatch
-  );
+    bindActionCreators(
+        {
+            logout,
+        },
+        dispatch
+    );
 
 export default NavBar = withRouter(connect(
     null,
     mapDispatchToProps
-  )(NavBar));
+)(NavBar));
 
